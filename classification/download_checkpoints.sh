650405891ee56f621216f2e84a953227b1ac35c3
Barbarioli <bbarbarioli@hotmail.com>
2019-06-13 17:12:46 -0500

    bash file updated

--- classification/download_checkpoints.sh
+++ classification/download_checkpoints.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./gdown.pl 'https://drive.google.com/open?id=1xYSQxa_RcCsId9Q_nUNss24-iAiK5ZaP' ['cifar_10_resnet.tar']   
+./gdown.pl 'https://drive.google.com/open?id=1xYSQxa_RcCsId9Q_nUNss24-iAiK5ZaP' ['cifar_10_resnet.tar']
+./gdown.pl 'https://drive.google.com/open?id=1cV5FAl2xaNulMj9Heo-CAWJKrMY7yJCT' ['cifar_10_resnet_5%.tar']
+ 
